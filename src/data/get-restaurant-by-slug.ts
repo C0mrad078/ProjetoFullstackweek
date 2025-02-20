cbe6b52d17aafdab8479b9d6f08a4a55b238bfca
jhonatan <jhonaatan81@gmail.com>
2025-02-20 20:02:30 -0300

    feat(page): create categories and products page

---
+++ src/data/get-restaurant-by-slug.ts
@@ -0,0 +1,6 @@
+////import { db } from "@/lib/prisma";
+
+//export const getRestaurantBySlug = async (slug: string) => {
+//const restaurant = await db.restaurant.findUnique({ where: { slug } }); //usado para chamar o bando de dados
+//return restaurant;
+//};
