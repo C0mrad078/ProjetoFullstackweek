bd8f59951aba56adcdf38f2f6412e3c03d5362c4
jhonatan <jhonaatan81@gmail.com>
2025-02-20 17:49:09 -0300

    Revert "feat: add unique constraint to restaurant slug"
    This reverts commit 08301490d4aba3bd24d084a19de0ece7034015e9.

--- src/data/get-restaurant-by-slug.ts
+++
@@ -1,6 +0,0 @@
-////import { db } from "@/lib/prisma";
-
-//export const getRestaurantBySlug = async (slug: string) => {
-//const restaurant = await db.restaurant.findUnique({ where: { slug } }); //usado para chamar o bando de dados
-//return restaurant;
-//};
